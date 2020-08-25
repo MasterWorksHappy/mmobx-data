985c534ca9501ab92cedf959738ad5825071a22b
Michele <MasterWorksHappy@gmail.com>
2020-08-25 10:51:26 -0400

    works on macos

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Notes
+
 # Inspiration:
 
 https://stackoverflow.com/questions/34437900/how-to-load-npm-modules-in-aws-lambda
+
+https://thetrevorharmon.com/blog/how-to-use-s3-select-to-query-json-in-node-js
--- src/csvToJSON.js
+++ src/csvToJSON.js
@@ -1,28 +1,36 @@
-require('dotenv').config();
-const AWS = require('aws-sdk');
-const csv = require('csvtojson');
+const csv = require("csvtojson");
+const AWS = require("aws-sdk");
 const S3 = new AWS.S3();
 
-const bucketName = 'mmobx.data';
+const bucketName = "mmobx.data";
 const files = [
-  'DB_Data_20200824 - Amenities',
-  'DB_Data_20200824 - Gallery',
-  'DB_Data_20200824 - Guide',
-  'DB_Data_20200824 - Pages',
-  'DB_Data_20200824 - Parts',
-  'DB_Data_20200824 - Post',
-  'DB_Data_20200824 - Rules'
-  ];
+  "DB_Data_20200824 - Amenities",
+  "DB_Data_20200824 - Gallery",
+  "DB_Data_20200824 - Guide",
+  "DB_Data_20200824 - Pages",
+  "DB_Data_20200824 - Parts",
+  "DB_Data_20200824 - Post",
+  "DB_Data_20200824 - Rules",
+];
 
-async function csvToJSON() {
+const csvToJSON = async () => {
   // get csv file and create stream
-  files.forEach((file) => {
-    const stream = S3.getObject(bucketName, `${filename}.csv`).createReadStream();
+  files.forEach(async (file) => {
+    const params = {
+      Bucket: bucketName,
+      Key: `${file}.csv`,
+    };
+    const stream = S3.getObject(params).createReadStream();
     // convert csv file (stream) to JSON format data
     const json = await csv().fromStream(stream);
-    console.log(json);
-
-  })
+    params["Body"] = JSON.stringify(json);
+    (params["Key"] = `${file}.json`), console.log(json);
+    S3.putObject(params, function (err, data) {
+      if (err) console.log(err, err.stack);
+      // an error occurred
+      else console.log(data); // successful response
+    });
+  });
 };
 
 csvToJSON();
